a3ad24d79efb64db3ea8330d0617e43617041bf3
scarfaced00 <jazmyne@naver.com>
2020-10-29 12:45:51 +0900

    ex2-2

--- main.c
+++ main.c
@@ -5,16 +5,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
+	int i, average;
+	int sum;
 	int grade[SIZE];
-	printf("5명의 점수를 입력하세요. \n");
-	
-	for(i=0;i<SIZE;i++)
-		scanf("%d",&grade[i]);
+
 	
 	for(i=0;i<SIZE;i++)
 	{
-		printf("grade[%d] = %d\n",i, grade[i]);
+		printf("학생 성적을 입력하세요: ");
+		scanf("%d",&grade[i]);
+		sum+=grade[i];
 	}
+	average=sum/SIZE;
+	printf("\t성적 평균: %d\n",average);
 	return 0;
 }
